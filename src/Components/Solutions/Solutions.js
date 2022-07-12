32c3dcc6437214ee9758602db6c46b0912987344
konf1nement <clearddcreations@gmail.com>
2022-07-12 15:16:18 -0400

    about page complete

---
+++ src/Components/Solutions/Solutions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Solutions = () => {
+  return (
+    <div className="solutions">
+        <div className="solutions-header">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Solutions
